d643d321d701e0b9405e339b03fbcce4091c91a1
DhuumMe <benoit.abelard@gmail.com>
2024-08-15 16:01:29 +0200

    Initial commit

---
+++ src/main/java/net/dhuumme/education/EducationClient.java
@@ -0,0 +1,10 @@
+package net.dhuumme.education;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class EducationClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/dhuumme/education/EducationDataGenerator.java
@@ -0,0 +1,11 @@
+package net.dhuumme.education;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class EducationDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
